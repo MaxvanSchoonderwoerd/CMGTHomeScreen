c21d6ed7878049e6f0e4931725005d3250fa5355
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-01-24 14:59:17 +0100

    You can now add your own links!

---
+++ js/createLink.js
@@ -0,0 +1,22 @@
+function updateVar() {
+    let url = document.getElementById('url').value;
+    let img = document.getElementById('img').value;
+    alert(url);
+
+    let place = document.getElementById('test');
+
+    place.insertAdjacentHTML('beforeend', ' <div class="card">\n' +
+        '                <a href="' + url + '">\n' +
+        '                    <img src="' + img + '" alt=" "\n' +
+    '                         class="linkImage image">\n' +
+    '                    <h3>' + url + '</h3>\n' +
+    '                </a>\n' +
+    '            </div>'
+)
+    ;
+}
+
+
+document.getElementById("createLink").addEventListener("click", function () {
+    updateVar();
+});
