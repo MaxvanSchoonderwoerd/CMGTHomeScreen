c2881f1d4129843f3b5217952603e68750828af3
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-01-24 16:11:36 +0100

    improved styling for the form, also made it mobile compatible

--- js/createLink.js
+++ js/createLink.js
@@ -2,21 +2,20 @@ function updateVar() {
     let url = document.getElementById('url').value;
     let img = document.getElementById('img').value;
     let name = document.getElementById('name').value;
-    alert(url);
 
     let place = document.getElementById('test');
 
-    place.insertAdjacentHTML('beforeend', ' <div class="card">\n' +
+    place.insertAdjacentHTML('beforeend', ' ' +
+        '            <div class="card">\n' +
         '                <a href="' + url + '">\n' +
         '                    <img src="' + img + '" alt=" "\n' +
-    '                         class="linkImage image">\n' +
-    '                    <h3>' + name + '</h3>\n' +
-    '                </a>\n' +
-    '            </div>'
-)
+        '                         class="linkImage image">\n' +
+        '                    <h3>' + name + '</h3>\n' +
+        '                </a>\n' +
+        '            </div>'
+    )
     ;
 }
-
 
 document.getElementById("createLink").addEventListener("click", function () {
     updateVar();
