b5555fcbe8a893c0fbb9047301f352c60e9d21da
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-03-30 15:01:10 +0200

    Improved the js to modern standards

--- js/createLink.js
+++ js/createLink.js
@@ -1,28 +1,42 @@
 window.addEventListener('load', init);
 
-function init(){
-    document.getElementById("createLink").addEventListener("click", function () {
-        updateVar();
-    });
+let button = document.getElementById("createLink");
+let url = document.getElementById('url');
+let img = document.getElementById('img');
+let name = document.getElementById('name');
+let container = document.getElementById('container');
+
+function init() {
+    button.addEventListener("click", formSubmitHandler);
 }
 
-function updateVar() {
-    let url = document.getElementById('url').value;
-    let img = document.getElementById('img').value;
-    let name = document.getElementById('name').value;
+function formSubmitHandler(e) {
+    e.preventDefault();
+    let $url = url.value;
+    let $img = img.value;
+    let $name = name.value;
 
-    let place = document.getElementById('test');
+    if ($url != "" || $img !="" || $name !="") {
+        let cardElement = document.createElement("div");
+        cardElement.className = "card";
 
-    place.insertAdjacentHTML('beforeend', ' ' +
-        '            <div class="card">\n' +
-        '                <a href="' + url + '">\n' +
-        '                    <img src="' + img + '" alt=" "\n' +
-        '                         class="linkImage image">\n' +
-        '                    <h3>' + name + '</h3>\n' +
-        '                </a>\n' +
-        '            </div>'
-    )
-    ;
+        let linkElement = document.createElement("a");
+        linkElement.href = $url;
+
+        let imageElement = document.createElement("img");
+        imageElement.className = "linkImage image";
+        imageElement.src = $img;
+
+        let h3Element = document.createElement("h3");
+        h3Element.innerHTML = $name;
+
+        container.appendChild(cardElement);
+        cardElement.appendChild(linkElement);
+        linkElement.appendChild(imageElement);
+        linkElement.appendChild(h3Element);
+    }
+    else
+    {
+        console.log("form is empty");
+    }
 }
-
-
